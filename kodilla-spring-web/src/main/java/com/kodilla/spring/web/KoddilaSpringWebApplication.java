e07f115eda7be6fc6d4aa374787910817ed9ffc2
darmoz <dariusz.mozgowoj@gmail.com>
2017-11-12 22:09:57 +0100

    commit after creating template for spring web application

---
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KoddilaSpringWebApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoddilaSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoddilaSpringWebApplication.class, args);
+	}
+}
