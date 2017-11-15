e2fae04f936287e0e4759882681e36b8134ec16d
darmoz <dariusz.mozgowoj@gmail.com>
2017-11-15 14:37:45 +0100

    commit after prototype challenge

---
+++ kodilla-patterns/src/main/java/prototypeChallenge/Prototype.java
@@ -0,0 +1,8 @@
+package prototypeChallenge;
+
+public class Prototype<T> implements Cloneable {
+    @Override
+    public T clone() throws CloneNotSupportedException {
+        return (T)super.clone();
+    }
+}
---
+++ kodilla-patterns/src/test/java/prototypeChallenge/LibraryTestSuite.java
@@ -0,0 +1,41 @@
+package prototypeChallenge;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+        //given
+        Library library = new Library("Library 1");
+        IntStream.iterate(1, n-> n+1)
+                .limit(5)
+                .forEach(n->library.getBooks().add(new Book("title"+n,"author"+n,
+                        LocalDate.of(2007, 04,01+n))));
+        //when
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Library 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        //then
+        System.out.println(library);
+        System.out.println(clonedLibrary);
+        System.out.println(deepClonedLibrary);
+
+    }
+
+}
