f923d52f52a8632bb06cf18fafb90382266cd79f
darmoz <dariusz.mozgowoj@gmail.com>
2018-01-03 17:26:47 +0100

    after extract gameLogic() method from while loop

--- kodilla-rps/src/main/java/GameEvaluator.java
+++ kodilla-rps/src/main/java/GameEvaluator.java
@@ -22,6 +22,36 @@ public class GameEvaluator {
         System.out.println("Actual AI Score: " + aiScore);
     }
 
+    private void gameLogic(User user, AI ai) throws IOException {
+        int colNum = user.getGameObject();
+        if (colNum > 2) {
+            if (colNum == 3) {
+                gameResource.close();
+                setGameDefaultValues();
+                evaluateGame();
+            } else if (colNum == 4) {
+                ai.forUserGameCancelation();
+                gameStats();
+                end = true;
+            }
+        } else if(colNum<=2) {
+            int rowNum = ai.getGameObject();
+            if (matrix[rowNum][colNum].equals("U")) {
+                userScore = userScore + 1;
+                System.out.println("You win!");
+                gameStats();
+            } else if (matrix[rowNum][colNum].equals("C")) {
+                aiScore = aiScore + 1;
+                System.out.println("AI win!");
+                gameStats();
+            } else if (matrix[rowNum].equals(matrix[colNum])) {
+                System.out.println("Tie!");
+                gameStats();
+            }
+            gameCounter++;
+        }
+    }
+
     public void evaluateGame() throws IOException {
         GameServices rpsGame = new GameServices();
         gameResource = getClass().getResourceAsStream("/inputData.txt");
@@ -31,33 +61,7 @@ public class GameEvaluator {
         setGameDefaultValues();
         System.out.println("Get started!!");
         while (!end && gameCounter < 10) {
-            int colNum = user.getGameObject();
-            if (colNum > 2) {
-                if (colNum == 3) {
-                    gameResource.close();
-                    setGameDefaultValues();
-                    evaluateGame();
-                } else if (colNum == 4) {
-                    ai.forUserGameCancelation();
-                    gameStats();
-                    end = true;
-                }
-            } else if(colNum<=2) {
-                int rowNum = ai.getGameObject();
-                if (matrix[rowNum][colNum].equals("U")) {
-                    userScore = userScore + 1;
-                    System.out.println("You win!");
-                    gameStats();
-                } else if (matrix[rowNum][colNum].equals("C")) {
-                    aiScore = aiScore + 1;
-                    System.out.println("AI win!");
-                    gameStats();
-                } else if (matrix[rowNum].equals(matrix[colNum])) {
-                    System.out.println("Tie!");
-                    gameStats();
-                }
-                gameCounter++;
-            }
+            gameLogic(user, ai);
 
         }
     }
